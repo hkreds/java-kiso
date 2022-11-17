454c992b328953b3ac8857682a480c28d24c2d95
vsckawa <109258421+hkreds@users.noreply.github.com>
2022-11-17 12:19:27 +0900

    [Finish]4章

---
+++ Chapter04.java
@@ -0,0 +1,10 @@
+public class Chapter04 {
+  public static void main(String[] args) {
+    String str = "hk";
+    System.out.println(str);
+    str = "football";
+    System.out.println(str);
+    final int LUCKY_NUMBER = 10;
+    System.out.println(LUCKY_NUMBER);
+  }
+}
