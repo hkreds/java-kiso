4a2f9c6f9610aef1d16044e6c29dd3b9c4d3918b
vsckawa <109258421+hkreds@users.noreply.github.com>
2022-11-17 14:44:49 +0900

    [Finish]5章

---
+++ Chapter05.java
@@ -0,0 +1,20 @@
+public class Chapter05 {
+  public static void main(String[] args) {
+    long int1 = 1000000000000L;
+    System.out.println(int1);
+    double int2 = 2.0;
+    System.out.println(int2);
+    boolean boolean1 = true;
+    System.out.println(boolean1);
+
+    Float int3 = 3.0f;
+    System.out.println(int3);
+    Integer int4 = 4;
+    System.out.println(int4);
+    Character string1 = 'あ';
+    System.out.println(string1);
+
+    int[] array = {10,20,30,40,50};
+    System.out.println(array[3]);
+  }
+}
